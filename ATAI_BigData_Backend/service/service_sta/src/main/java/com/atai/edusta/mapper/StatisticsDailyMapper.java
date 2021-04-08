26726b13b288d340df51799c277505a9a43220c2
1731lin <35423982+1731lin@users.noreply.github.com>
2021-04-08 11:57:34 +0800

    Add files via upload

---
+++ ATAI_BigData_Backend/service/service_sta/src/main/java/com/atai/edusta/mapper/StatisticsDailyMapper.java
@@ -0,0 +1,13 @@
+package com.atai.edusta.mapper;
+
+import com.atai.edusta.entity.StatisticsDaily;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 网站统计日数据 Mapper 接口
+ * @author ZengJinming
+ * @since 2020-04-13
+ */
+public interface StatisticsDailyMapper extends BaseMapper<StatisticsDaily> {
+
+}
